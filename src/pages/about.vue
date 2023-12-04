945f582fd5ff528510ffdd62ef11babf6fe67cd9
ahmaddmsy <bidni.musyafa@gmail.com>
2023-12-05 00:23:07 +0700

    first init github

---
+++ src/pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Ahmad Bidni Musyafa</div>
+  <p> 22106050063 </p>
+  <p> Membuat Login dengan JavaScript</p>
+</template>
---
+++ src/pages/restricted.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+  <p>Ahmad Bidni Musyafa</p>
+  <P>22106050063</P>
+  <p>Informatika 2022</p>
+  <p>Semester 3</p>
+  <p>UIN Sunan Kalijaga</p>
+  </div>
+</template>
